7f047236c28abd1da06fd14bf2326ed22ba07f0a
m.reza120@gmail.com <m.reza120@gmail.com>
2021-04-19 22:04:16 +0430

    Commit Lsst update before moving to docker

--- app2.py
+++ app2.py
@@ -1,25 +1,32 @@
-from flask import Flask , Response , request
+from flask import Flask , Response , request ,jsonify
 import redis
 
 
-# r = redis.StrictRedis(
-# host='redis',
-# port=6379,
-# charset="utf-8", 
-# decode_responses=True ,
-# password='')
+r = redis.StrictRedis(
+host='127.0.0.1',
+port=6379,
+charset="utf-8", 
+decode_responses=True ,
+password='')
 
 app = Flask(__name__)
-@app.route('/1' , methods=['GET'] )
+@app.route('/1/' , methods=['GET'] )
 def Say_Hello():
     # name2 = r.get('reza')
     # return requests
     # print("Hello there")
-    
-    print(request.json)
-    return "HIII"
-    # return response(status=200) 
+    # r = request.get_data
+    # print(request.json)
+    a = r.keys('*asdasddname*')
+
+    # return jsonify(Query_Result = r.get('firstname'))
+    # return response(status=200)
+    # return r.get('a')
+    # return request.query_string
+    return jsonify(len(a))
 
 if __name__ == "__main__":
     app.run("0.0.0.0",5000)
+
+
  
